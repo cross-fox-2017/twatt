43f7c04c38a89358ec2dd024bf564f00f1a46e78
yonisetiawan <yonisetiawan1990@gmail.com>
2017-02-01 12:03:19 +0700

    Search Twiiter Keyword

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
-
+var Twit = require('twit')
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+const config = require('../config/auth');
+
+
+var T = new Twit({
+  consumer_key:         config.twitter.consumer_key,
+  consumer_secret:      config.twitter.consumer_secret,
+  access_token:         config.twitter.access_token,
+  access_token_secret:  config.twitter.access_token_secret,
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+})
+
+
+router.get('/search', function(req, res, next) {
+  T.get('search/tweets', { q: req.query.kata, count: 4 }, function(err, data, response) {
+    // console.log(data.statuses)
+    res.send(data)
+  })
 });
+
+
 
 module.exports = router;
