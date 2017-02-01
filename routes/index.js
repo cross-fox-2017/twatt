8bde5bd72d937c7c6c6f6eab6b2195e1f7b962fe
Raditya <arya.raditya26@gmail.com>
2017-02-01 12:16:15 +0700

    switch to npm twitter (from npm-oauth)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
-var express = require('express');
-var router = express.Router();
+////////// TWAT CHALLENGE ///////////////
+const express = require('express');
+const router = express.Router();
+const twitter = require('twitter');
+var config = require('../config.json')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var client = new Twitter({
+  consumer_key: config.consumer_key,
+  consumer_secret: config.consumer_secret,
+  access_token_key: config.access_token_key,
+  access_token_secret: config.access_token_secret
 });
 
+router.get('/search', function (res, req) {
+  var search_query = req.query.q
+
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      res.send(tweets);
+    }
+  })
+
+})
+
+
+client.get('api/twitter/callback', function(error, tweets, response) {
+    res.send(tweets);
+})
+
+
 module.exports = router;
+////////////////////////////////////////////
