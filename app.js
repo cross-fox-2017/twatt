83fd13daf002bfc291c5d44128eadf14e96b098a
Irsan Sebastian <irsansebastianc4@gmail.com>
2017-02-01 12:03:23 +0700

    git ignore app.js dan config example untuk testing

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require('express')
 const router = express.Router()
 
-const twitter = require('twitter')
+const Twitter = require('twitter')
+
+const config = require('./config');
 
 const app = express()
 
-router.get('/',function(req, res){
+var client = new Twitter({
+  consumer_key: config.consumer_key,
+  consumer_secret: config.consumer_secret,
+  access_token_key: config.access_token_key,
+  access_token_secret: config.access_token_secret
+});
 
-      res.send("hello world !")
+router.get('/search',function(req, res) {
+
+  console.log({debug: req.query});
+  console.log({client})
+
+  const searchQuery = req.query.q;
+
+  client.get('search/tweets', {q: searchQuery}, function(error, tweets, response) {
+    const result = JSON.parse(response.body)
+    console.log({result})
+    res.send(tweets)
+  })
+
 })
 
 app.use('/', router)
