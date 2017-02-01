9944a560f576ef3e7e5e1833ab75754c34243063
Alexander Hendrawan tjahja <alexanderhendrawan@gmail.com>
2017-02-01 12:17:38 +0700

    perbaikan param

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 const tweet = require('../helper/twitterHelper');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/:search', function(req, res, next) {
+  console.log(req.params.search);
   tweet.getTweet(req.params.search, function(data){
     res.json(data)
   })
